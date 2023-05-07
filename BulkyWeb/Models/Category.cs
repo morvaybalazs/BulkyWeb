bf7d3d31119d9daba908b5bbcd946e4d3445920c
Balazs Morvay <morvaybalazs@gmail.com>
2023-05-07 14:03:56 +0100

    create database and first table

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } // Removed '?' to make it non-nullable
+        public int DisplayOrder { get; set; }
+    }
+}
+
